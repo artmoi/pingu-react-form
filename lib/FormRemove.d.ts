97b3efd66cd40db8589fcb515f67fabd38dab822
Ryan Mayberry <ryan@artmail.ca>
2019-08-19 08:47:33 -0500

    add lib

---
+++ lib/FormRemove.d.ts
@@ -0,0 +1,7 @@
+import React from "react";
+export interface ComponentProps {
+}
+export declare type Props = ComponentProps;
+export declare class FormRemove extends React.Component<Props> {
+    render(): React.ReactNode;
+}
